09ab2ba4059039b7c6235d1a5dbffd9c6afe5421
Hadhuuu <145408376+Hadhuuu@users.noreply.github.com>
2023-11-21 21:01:35 +0700

    Add files via upload
    perubahan terakhir

--- Bioskop12.java
+++ Bioskop12.java
@@ -1,7 +1,7 @@
 public class Bioskop12 {
 
     public static void main(String[] args) {
-        String[][] penonton = new String[4][2];
+        String[][] penonton = new String[4][3];
         penonton[0][0]="Amin";
         penonton[0][1]="Bena";
         penonton[1][0]="Candra";
@@ -17,8 +17,23 @@ public class Bioskop12 {
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        for (int i = 0; i<penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " +penonton[i].length);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris : " + barisPenonton.length);
         }
+        // for (int i = 0; i<penonton.length; i++) {
+        //     System.out.println("Panjang baris ke-" + (i+1) + ": " +penonton[i].length);
+        // }
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (int i=0; i<penonton.length;i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " +String.join(", ",penonton[i]));
+        }
+        // for(String i : penonton[2]) {
+        //     System.out.println(i);
+        // }
+        // for (int i=0;i<penonton[2].length; i++) {
+        //     System.out.println(penonton[2][i]);
+        // }
     }
 }
