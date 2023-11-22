4af2d45d3780e54437fb6876b11f56a22d892b95
Hadhuuu <145408376+Hadhuuu@users.noreply.github.com>
2023-11-22 22:02:00 +0700

    Add files via upload

---
+++ Numbers12.java
@@ -0,0 +1,10 @@
+public class Numbers12 {
+
+    public static void main(String[] args) {
+        int[][] myNumbers = new int [3][];
+        myNumbers[0] = new int[5];
+        myNumbers[0] = new int[3];
+        myNumbers[0] = new int[1];
+        
+    }
+}
